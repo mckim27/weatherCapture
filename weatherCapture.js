94df209c84b2305c043cd206498db83bf755fa6c
Myung Cheol Kim <bluevoice27@gmail.com>
2015-03-19 15:26:20 +0900

    Update weatherCapture.js

--- weatherCapture.js
+++ weatherCapture.js
@@ -1,3 +1,5 @@
+
+// module 세팅.
 try {
     var Spooky = require('spooky');
 } catch (e) {
@@ -23,12 +25,15 @@ var spooky = new Spooky({
             e.details = err;
             throw e;
         }
+        // 다음 날씨예보 페이지를 연다.
         spooky.start('http://search.daum.net/search?w=tot&q=%EC%84%9C%EC%9A%B8%20%EA%B4%91%EC%A7%84%EA%B5%AC%20%EC%9E%90%EC%96%911%EB%8F%99%20%EB%82%A0%EC%94%A8');
-
+        
+        //해당 dom 객체만 스크린샷으로 저장함.
         spooky.then(function () {
           this.captureSelector('daum_weather.png', '#weatherColl');
         });
         
+        //rabbitmq에 메시지를 보낼 이벤트를 발생시킴.
         spooky.then(function () {
             var data = {'queue' : 'email', 'addr':'youremail'};
             this.emit('amqp', data);
@@ -54,6 +59,7 @@ spooky.on('console', function (line) {
 });
 */
 
+// rabbitmq에 메시지 전달하는 함수.
 spooky.on('amqp', function (data) {
     //console.log(data);
     amqp.connect('amqp://your_mqhost').then(function(conn) {
@@ -73,6 +79,7 @@ spooky.on('amqp', function (data) {
   }).then(null, console.warn);
 });
 
+// log 함수.
 spooky.on('log', function (log) {
     if (log.space === 'remote') {
         console.log(log.message.replace(/ \- .*/, ''));
